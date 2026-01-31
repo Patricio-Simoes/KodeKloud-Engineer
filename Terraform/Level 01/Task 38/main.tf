966c31108e8146fa35d48aef5fccd0de24e9fae3
patricio-simoes <100965280+Patricio-Simoes@users.noreply.github.com>
2026-01-31 18:22:53 +0000

    Terraform - Level 01 - Task 39

--- Terraform/Level 01/Task 38/main.tf
+++ Terraform/Level 01/Task 38/main.tf
@@ -1,20 +1,3 @@
-resource "aws_iam_role" "role" {
-  name = "iamrole_jim"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = {
-    Name = "iamrole_jim"
-  }
+resource "aws_iam_user" "this" {
+  name = var.KKE_user
 }
---
+++ Terraform/Level 01/Task 38/variables.tf
@@ -0,0 +1,5 @@
+variable "KKE_user" {
+  type        = string
+  description = "Name of the IAM User"
+  default     = "iamuser_mark"
+}
